45cdfc000039c45991a05365fb653323de4cfeb2
wmdwjddyd6 <wmdwjddyd6@naver.com>
2020-12-27 21:38:39 +0900

    php/js 출력

---
+++ project1/phpjs/1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>php</h1>
+    <?php
+      echo "Hello world";
+    ?>
+    <h1>JavaScript</h1>
+    <script>
+        document.write("Hello world");
+    </script>
+</body>
+</html>
