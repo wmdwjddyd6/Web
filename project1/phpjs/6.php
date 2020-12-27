3c156fc7458af16dd4626a233ea44a556231bf9a
wmdwjddyd6 <wmdwjddyd6@naver.com>
2020-12-27 21:41:11 +0900

    php/js 조건문

---
+++ project1/phpjs/6.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>javascript</h1>
+    <script>
+        result = (1==2);
+        if(result){
+            document.write("참");
+        } else{
+            document.write("거짓");
+        }
+    </script>
+    <h1>php</h1>
+    <?php
+        $result = (2==2);
+        if($result){
+            echo "참";
+        } else {
+            echo "거짓";
+        }
+    ?>
+</body>
+</html>
