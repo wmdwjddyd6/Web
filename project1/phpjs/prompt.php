082a61d98da02241e1faefbf293d8373c50923a6
wmdwjddyd6 <wmdwjddyd6@naver.com>
2020-12-28 03:52:48 +0900

    prompt데이터 받기

---
+++ project1/phpjs/prompt.php
@@ -0,0 +1,16 @@
+<!--프롬프트(비밀번호 입력받기)-->
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <script>
+        password = prompt("비밀번호");
+        if(password == 1111){
+            document.write("안녕하세요");
+        } else{
+            document.wirte("error");
+        }
+    </script>
+</body>
+</html>
