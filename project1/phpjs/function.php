dc4ac9e11ac3bea369248b42781a66889b341bb2
wmdwjddyd6 <wmdwjddyd6@naver.com>
2020-12-28 04:28:49 +0900

    php/js 함수

---
+++ project1/phpjs/function.php
@@ -0,0 +1,23 @@
+<!--12-->
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        function a(input){
+            return input+1;
+        }
+        document.write(a(6));
+    </script>
+
+    <h1>php</h1>
+    <?php
+        function a($input){
+            return $input+1;
+        }
+        echo a(6);
+    ?>
+</body>
+</html>
