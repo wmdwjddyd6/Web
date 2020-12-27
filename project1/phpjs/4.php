80fef59ec93b06ae7e6d0e94b9ca17c917032c84
wmdwjddyd6 <wmdwjddyd6@naver.com>
2020-12-27 21:39:39 +0900

    php/js 변수

---
+++ project1/phpjs/4.php
@@ -0,0 +1,22 @@
+<?php
+$name = "min"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>javascript</h1>
+    <script>
+        name = "min";
+        document.write("안녕하세요. " +name);
+    </script>
+    <h1>php</h1>
+    <?php
+        echo "안녕하세요. ".$name;
+    ?>
+</body>
+</html>
