6347947f59581510336b16069ce4240e2f64e7ac
wmdwjddyd6 <wmdwjddyd6@naver.com>
2020-12-28 04:01:02 +0900

    php/js 배열

---
+++ project1/phpjs/array.php
@@ -0,0 +1,19 @@
+<!--10-->
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>php</h1>
+    <?php
+        $list = array("one","two","three");
+        echo $list[2];
+    ?>
+
+    <h1>JavaScript</h1>
+    <script>
+        list = new Array("one", "two", "three");
+        document.write(list[0]);
+    </script>
+</body>
+</html>
