5b6cf151efac210ebfde821641f41934b40ba38c
wmdwjddyd6 <wmdwjddyd6@naver.com>
2020-12-27 21:40:27 +0900

    php/js bool

---
+++ project1/phpjs/5.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>javascript</h1>
+    <script>
+        document.write(2==1);
+    </script>
+    <h1>php</h1>
+    <?php
+    var_dump(1==1);
+    ?>
+</body>
+</html>
