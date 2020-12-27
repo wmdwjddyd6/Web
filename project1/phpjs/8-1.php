c0cd2116c5b2ae7f3d8b02c1e70fc1c787a87d72
wmdwjddyd6 <wmdwjddyd6@naver.com>
2020-12-28 03:54:11 +0900

    데이터 형태에 따른 페이지표시

---
+++ project1/phpjs/8-1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <form action="8-2.php">
+        <p>비밀번호를 입력해주세요.</p>
+        <input type="text" name="password">
+        <input type="submit">
+    </form>
+</body>
+</html>
---
+++ project1/phpjs/8-2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <?php
+        $password = $_GET["password"];
+        if($password == "1111"){
+            echo "hi";
+        }else{
+            echo "error";
+        }
+    ?>
+</body>
+</html>
